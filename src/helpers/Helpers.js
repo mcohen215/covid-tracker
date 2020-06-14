37808667484f1ead4e140657d09638104e35ac4c
Andrew Smith <andrewts129@gmail.com>
2020-06-14 18:59:14 -0400

    using object literals because that's just what you do dude

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -5,73 +5,74 @@ export const USCurrent = 'https://covidtracking.com/api/v1/us/current.json';
 export const USHistoric = 'https://covidtracking.com/api/v1/us/daily.json';
 
 //Dictionary which maps state full name to 2 letter abbreviation (for API link purposes)
-export const stateMap = {};
-stateMap['Alabama'] = 'al';
-stateMap['Alaska'] = 'ak';
-stateMap['Arizona'] = 'az';
-stateMap['Arkansas'] = 'ar';
-stateMap['California'] = 'ca';
-stateMap['Colorado'] = 'co';
-stateMap['Connecticut'] = 'ct';
-stateMap['Delaware'] = 'de';
-stateMap['Flordia'] = 'fl';
-stateMap['Georgia'] = 'ga';
-stateMap['Hawaii'] = 'hi';
-stateMap['Idaho'] = 'id';
-stateMap['Illinois'] = 'il';
-stateMap['Indiana'] = 'in';
-stateMap['Iowa'] = 'ia';
-stateMap['Kansas'] = 'ks';
-stateMap['Kentucky'] = 'ky';
-stateMap['Louisiana'] = 'la';
-stateMap['Maine'] = 'me';
-stateMap['Maryland'] = 'md';
-stateMap['Massachussetts'] = 'ma';
-stateMap['Michigan'] = 'mi';
-stateMap['Minnesota'] = 'mn';
-stateMap['Mississippi'] = 'ms';
-stateMap['Missouri'] = 'mo';
-stateMap['Montana'] = 'mt';
-stateMap['Nebraska'] = 'ne';
-stateMap['Nevada'] = 'nv';
-stateMap['New Hampshire'] = 'nh';
-stateMap['New Jersey'] = 'nj';
-stateMap['New Mexico'] = 'nm';
-stateMap['New York'] = 'ny';
-stateMap['North Carolina'] = 'nc';
-stateMap['North Dakota'] = 'nd';
-stateMap['Ohio'] = 'oh';
-stateMap['Oklahoma'] = 'ok';
-stateMap['Oregon'] = 'or';
-stateMap['Pennsylvania'] = 'pa';
-stateMap['Rhode Island'] = 'ri';
-stateMap['South Carolina'] = 'sc';
-stateMap['South Dakota'] = 'sd';
-stateMap['Tennessee'] = 'tn';
-stateMap['Texas'] = 'tx';
-stateMap['Utah'] = 'ut';
-stateMap['Vermont'] = 'vt';
-stateMap['Virginia'] = 'va';
-stateMap['Washington'] = 'wa';
-stateMap['West Virginia'] = 'wv';
-stateMap['Wisconsin'] = 'wi';
-stateMap['Wyoming'] = 'wy';
+export const stateMap = {
+    'Alabama': 'al',
+    'Alaska': 'ak',
+    'Arizona': 'az',
+    'Arkansas': 'ar',
+    'California': 'ca',
+    'Colorado': 'co',
+    'Connecticut': 'ct',
+    'Delaware': 'de',
+    'Flordia': 'fl',
+    'Georgia': 'ga',
+    'Hawaii': 'hi',
+    'Idaho': 'id',
+    'Illinois': 'il',
+    'Indiana': 'in',
+    'Iowa': 'ia',
+    'Kansas': 'ks',
+    'Kentucky': 'ky',
+    'Louisiana': 'la',
+    'Maine': 'me',
+    'Maryland': 'md',
+    'Massachussetts': 'ma',
+    'Michigan': 'mi',
+    'Minnesota': 'mn',
+    'Mississippi': 'ms',
+    'Missouri': 'mo',
+    'Montana': 'mt',
+    'Nebraska': 'ne',
+    'Nevada': 'nv',
+    'New Hampshire': 'nh',
+    'New Jersey': 'nj',
+    'New Mexico': 'nm',
+    'New York': 'ny',
+    'North Carolina': 'nc',
+    'North Dakota': 'nd',
+    'Ohio': 'oh',
+    'Oklahoma': 'ok',
+    'Oregon': 'or',
+    'Pennsylvania': 'pa',
+    'Rhode Island': 'ri',
+    'South Carolina': 'sc',
+    'South Dakota': 'sd',
+    'Tennessee': 'tn',
+    'Texas': 'tx',
+    'Utah': 'ut',
+    'Vermont': 'vt',
+    'Virginia': 'va',
+    'Washington': 'wa',
+    'West Virginia': 'wv',
+    'Wisconsin': 'wi',
+    'Wyoming': 'wy'
+};
 
 //Array containing all 50 states (for the dropdown menu)
 export const states = Object.keys(stateMap);
 
 //Dictionary which maps radio button options to what the field name is in the JSON repsonse from the API, and vice versa.
-export const JSONFieldMap = {};
-JSONFieldMap['Total Cases'] = 'positive';
-JSONFieldMap['Total Deaths'] = 'death';
-JSONFieldMap['Total Tests'] = 'total';
-JSONFieldMap['New Cases'] = 'positiveIncrease';
-JSONFieldMap['New Deaths'] = 'deathIncrease';
-JSONFieldMap['New Tests'] = 'totalTestResultsIncrease';
-
-JSONFieldMap['positive'] = 'Total Cases';
-JSONFieldMap['death'] = 'Total Deaths';
-JSONFieldMap['total'] = 'Total Tests';
-JSONFieldMap['positiveIncrease'] = 'New Cases';
-JSONFieldMap['deathIncrease'] = 'New Deaths';
-JSONFieldMap['totalTestResultsIncrease'] = 'New Tests';
+export const JSONFieldMap = {
+    'Total Cases': 'positive',
+    'Total Deaths': 'death',
+    'Total Tests': 'total',
+    'New Cases': 'positiveIncrease',
+    'New Deaths': 'deathIncrease',
+    'New Tests': 'totalTestResultsIncrease',
+    'positive': 'Total Cases',
+    'death': 'Total Deaths',
+    'total': 'Total Tests',
+    'positiveIncrease': 'New Cases',
+    'deathIncrease': 'New Deaths',
+    'totalTestResultsIncrease': 'New Tests',
+};
